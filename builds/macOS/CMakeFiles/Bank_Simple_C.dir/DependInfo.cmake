5ea26663d4f49a8df7232cb23851a02bb4cad26e
livxy <action@github.com>
2023-05-25 00:38:15 +0000

    Add build outputs

---
+++ builds/macOS/CMakeFiles/Bank_Simple_C.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/runner/work/Simple-Bank/Simple-Bank/BankAccount.c" "CMakeFiles/Bank_Simple_C.dir/BankAccount.c.o" "gcc" "CMakeFiles/Bank_Simple_C.dir/BankAccount.c.o.d"
+  "/Users/runner/work/Simple-Bank/Simple-Bank/main.c" "CMakeFiles/Bank_Simple_C.dir/main.c.o" "gcc" "CMakeFiles/Bank_Simple_C.dir/main.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
